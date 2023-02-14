025b94da66f8a29e204cfe6a55ae73faf2e9a163
Abdurrahman-Ramadhan-PT <puterateguh777@gmail.com>
2023-02-14 14:59:10 +0700

    "coba test transisi hover pada mobile n laptop"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import Footer from "./components/footer/Footer";
 import Main from "./components/main/Main";
 import Navbar from "./components/navbar/Navbar";
-import "./style-app.css"
+import "./style-app.css";
+
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Main/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./style-footer.css"
+
+export default function Footer() {
+  return (
+    <footer>
+        <small>© 2023 by @abdrhmn.rmdhn</small>
+    </footer>
+  )
+}
